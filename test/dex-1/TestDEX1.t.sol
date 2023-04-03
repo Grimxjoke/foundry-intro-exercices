9d8cbbf53fce53b409fcc44b1b3ecb46716837d7
0x3agle <priyamsoni15@gmail.com>
2023-04-03 15:48:29 +0530

    intial commit

---
+++ test/dex-1/TestDEX1.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+/**
+@dev run "forge test --match-contract AC1" 
+*/
+contract TestCon is Test {
+    address deployer;
+    address user;
+
+    function setUp() public {}
+
+    function test_Attack() public {}
+}
